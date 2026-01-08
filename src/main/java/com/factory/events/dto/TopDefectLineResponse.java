ad07fcf783c60b277f7a530646ea64c05f18c314
Navinder Gill <nnavigill784@gmail.com>
2026-01-08 20:34:04 +0530

    add TopDefectLineResponse DTO with defect percentage calculation

---
+++ src/main/java/com/factory/events/dto/TopDefectLineResponse.java
@@ -0,0 +1,55 @@
+// TopDefectLineResponse.java
+package com.factory.events.dto;
+
+public class TopDefectLineResponse {
+
+    private String lineId;
+    private long totalDefects;
+    private long eventCount;
+    private double defectsPercent;
+
+    public TopDefectLineResponse() {
+    }
+
+    public TopDefectLineResponse(String lineId, long totalDefects, long eventCount) {
+        this.lineId = lineId;
+        this.totalDefects = totalDefects;
+        this.eventCount = eventCount;
+        this.defectsPercent = eventCount > 0
+                ? Math.round((totalDefects * 10000.0) / eventCount) / 100.0
+                : 0.0;
+    }
+
+    // Getters and Setters
+    public String getLineId() {
+        return lineId;
+    }
+
+    public void setLineId(String lineId) {
+        this.lineId = lineId;
+    }
+
+    public long getTotalDefects() {
+        return totalDefects;
+    }
+
+    public void setTotalDefects(long totalDefects) {
+        this.totalDefects = totalDefects;
+    }
+
+    public long getEventCount() {
+        return eventCount;
+    }
+
+    public void setEventCount(long eventCount) {
+        this.eventCount = eventCount;
+    }
+
+    public double getDefectsPercent() {
+        return defectsPercent;
+    }
+
+    public void setDefectsPercent(double defectsPercent) {
+        this.defectsPercent = defectsPercent;
+    }
+}
